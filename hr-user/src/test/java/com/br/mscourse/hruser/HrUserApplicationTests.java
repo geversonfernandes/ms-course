f9e8ea5e746b636c57e0c09949b7091902b222d1
geversonfernandes <gever.fernandes@gmail.com>
2023-10-22 02:43:16 -0300

    Projet hr-user created

---
+++ hr-user/src/test/java/com/br/mscourse/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mscourse.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

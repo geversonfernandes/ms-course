778d996fbb1a57ae518cbd5b9276dc239f7474bd
geversonfernandes <gever.fernandes@gmail.com>
2023-10-22 21:29:48 -0300

    UserRepository, UserResource, Zull config

---
+++ hr-user/src/main/java/com/br/mscourse/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.br.mscourse.hruser.repositories;
+
+import com.br.mscourse.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
